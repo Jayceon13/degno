706a5e9f456804215a775cbcf628ed36be97b7c7
Jayceon <jayceon310893@gmail.com>
2023-01-30 18:54:42 +0300

    header and video parallax

---
+++ src/components/HeaderDengo.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="header">
+    <div class="buttons-block">
+      <btn
+        class="buttons-header"
+      >
+        Главная
+      </btn>
+      <btn
+        class="buttons-header"
+      >
+        О нас
+      </btn>
+      <btn
+        class="buttons-header"
+      >
+        Наши Услуги
+      </btn>
+    </div>
+      <div class="logo-block">
+
+      </div>
+    <div class="buttons-block">
+        <btn
+          class="buttons-header"
+        >
+          Наши проекты
+        </btn>
+        <btn
+          class="buttons-header"
+        >
+          Партнёры
+        </btn>
+        <btn
+          class="buttons-header"
+        >
+          Контакты
+        </btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderDengo'
+}
+</script>
+
+<style scoped>
+  .header{
+    position: absolute;
+    display: flex;
+    width: 100vw;
+    z-index: 9999;
+    place-content: center;
+    padding: 64px 150px 0px;
+    align-items: center;
+  }
+
+ .buttons-block .buttons-header{
+    color: white;
+   transition: color 1s;
+   background: transparent;
+   margin: 0 1em;
+  }
+ .buttons-block .buttons-header:hover {
+   color: #4fdbe8;
+   background-color: transparent;
+   cursor: pointer;
+  }
+ .logo-block{
+   width: 288px;
+   height: 166px;
+ }
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,24 @@
 <template>
-  <q-page class="flex flex-center">
-    
+  <q-page class="">
+    <q-parallax :height="700" class="parallax">
+      <template v-slot:media>
+        <video :height="1200"  class="video-background" poster="https://cdn.quasar.dev/img/polina.jpg" playsinline autoplay loop muted>
+          <source type="video/mp4" src="/video/videobgInd.mp4">
+        </video>
+      </template>
+
+
+    </q-parallax>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
   </q-page>
 </template>
 
@@ -11,3 +29,14 @@ export default defineComponent({
   name: 'IndexPage'
 })
 </script>
+
+<style scoped>
+  .parallax{
+    z-index: -1;
+    width: 100vw;
+    height: 100vh;
+  }
+  .video-background{
+
+  }
+</style>
